4da529b8e800018ae99af8797208e5b45300a62d
hvrpuruma <harshavardhanpuruma@gmail.com>
2021-04-22 00:11:47 -0500

    Removed in-memory authentication and added data.sql, schema.sql files

--- src/main/java/in/harsha/springsecurityjdbc/SecurityConfiguration.java
+++ src/main/java/in/harsha/springsecurityjdbc/SecurityConfiguration.java
@@ -21,18 +21,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.jdbcAuthentication()
-				.dataSource(dataSource)
-				.withDefaultSchema()
-				.withUser(
-						User.withUsername("user")
-								.password("user")
-								.roles("USER")
-				)
-				.withUser(
-						User.withUsername("admin")
-								.password("admin")
-								.roles("ADMIN")
-				);
+				.dataSource(dataSource);
 	}
 
 	@Override
